56e86c6eee0a68f44a397c4d5cb765d8574c5b02
zacharyasmith <zach@medialusions.com>
2018-02-22 22:02:38 -0700

    Symbol table visitor complete.

--- CS453/Compiler Project/src/src/SymTableVisitor.java
+++ CS453/Compiler Project/src/src/SymTableVisitor.java
@@ -3,18 +3,9 @@ import visitor.*;
 
 import java.util.HashMap;
 
-public class SymTableVisitor<R,A> extends GJDepthFirst<R,A> {
-    // consider <String (for id), (something for types)>
-    public HashMap<String,IdentifierType> table = new HashMap<>();
+public class SymTableVisitor<R> extends GJDepthFirst<R,String> {
 
-    /**
-     * f0 -> MainClass()
-     * f1 -> ( TypeDeclaration() )*
-     * f2 -> <EOF>
-     */
-    public R visit(Goal n, A argu) {
-        return super.visit(n, argu);
-    }
+    public HashMap<String,IdentifierType> table = new HashMap<>();
 
     /**
      * f0 -> "class"
@@ -36,16 +27,9 @@ public class SymTableVisitor<R,A> extends GJDepthFirst<R,A> {
      * f16 -> "}"
      * f17 -> "}"
      */
-    public R visit(MainClass n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> ClassDeclaration()
-     *       | ClassExtendsDeclaration()
-     */
-    public R visit(TypeDeclaration n, A argu) {
-        return super.visit(n, argu);
+    public R visit(MainClass n, String argu) {
+        String id = n.f1.f0.tokenImage + "::main::";
+        return super.visit(n, id);
     }
 
     /**
@@ -56,8 +40,9 @@ public class SymTableVisitor<R,A> extends GJDepthFirst<R,A> {
      * f4 -> ( MethodDeclaration() )*
      * f5 -> "}"
      */
-    public R visit(ClassDeclaration n, A argu) {
-        return super.visit(n, argu);
+    public R visit(ClassDeclaration n, String argu) {
+        String id = n.f1.f0.tokenImage + "::";
+        return super.visit(n, id);
     }
 
     /**
@@ -70,7 +55,8 @@ public class SymTableVisitor<R,A> extends GJDepthFirst<R,A> {
      * f6 -> ( MethodDeclaration() )*
      * f7 -> "}"
      */
-    public R visit(ClassExtendsDeclaration n, A argu) {
+    public R visit(ClassExtendsDeclaration n, String argu) {
+        String id = n.f1.f0.tokenImage + "::";
         return super.visit(n, argu);
     }
 
@@ -78,16 +64,9 @@ public class SymTableVisitor<R,A> extends GJDepthFirst<R,A> {
      * f0 -> Type()
      * f1 -> Identifier()
      * f2 -> ";"
-     *
-     * Identifier:
-     *  * Grammar production:
-     * f0 -> ArrayType()
-     *       | BooleanType()
-     *       | IntegerType()
-     *       | Identifier()
      */
-    public R visit(VarDeclaration n, A argu) {
-        table.put(n.f1.f0.tokenImage, new IdentifierType(n.f0));
+    public R visit(VarDeclaration n, String argu) {
+        table.put(argu + n.f1.f0.tokenImage, new IdentifierType(n.f0));
         return super.visit(n, argu);
     }
 
@@ -106,338 +85,18 @@ public class SymTableVisitor<R,A> extends GJDepthFirst<R,A> {
      * f11 -> ";"
      * f12 -> "}"
      */
-    public R visit(MethodDeclaration n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> FormalParameter()
-     * f1 -> ( FormalParameterRest() )*
-     */
-    public R visit(FormalParameterList n, A argu) {
-        return super.visit(n, argu);
+    public R visit(MethodDeclaration n, String argu) {
+        // argu = <classname>::
+        String id = argu + n.f2.f0.tokenImage + "::";
+        return super.visit(n, id);
     }
 
     /**
      * f0 -> Type()
      * f1 -> Identifier()
      */
-    public R visit(FormalParameter n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> ","
-     * f1 -> FormalParameter()
-     */
-    public R visit(FormalParameterRest n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> ArrayType()
-     *       | BooleanType()
-     *       | IntegerType()
-     *       | Identifier()
-     */
-    public R visit(Type n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "int"
-     * f1 -> "["
-     * f2 -> "]"
-     */
-    public R visit(ArrayType n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "boolean"
-     */
-    public R visit(BooleanType n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "int"
-     */
-    public R visit(IntegerType n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> Block()
-     *       | AssignmentStatement()
-     *       | ArrayAssignmentStatement()
-     *       | IfStatement()
-     *       | WhileStatement()
-     *       | PrintStatement()
-     */
-    public R visit(Statement n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "{"
-     * f1 -> ( Statement() )*
-     * f2 -> "}"
-     */
-    public R visit(Block n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> Identifier()
-     * f1 -> "="
-     * f2 -> Expression()
-     * f3 -> ";"
-     */
-    public R visit(AssignmentStatement n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> Identifier()
-     * f1 -> "["
-     * f2 -> Expression()
-     * f3 -> "]"
-     * f4 -> "="
-     * f5 -> Expression()
-     * f6 -> ";"
-     */
-    public R visit(ArrayAssignmentStatement n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "if"
-     * f1 -> "("
-     * f2 -> Expression()
-     * f3 -> ")"
-     * f4 -> Statement()
-     * f5 -> "else"
-     * f6 -> Statement()
-     */
-    public R visit(IfStatement n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "while"
-     * f1 -> "("
-     * f2 -> Expression()
-     * f3 -> ")"
-     * f4 -> Statement()
-     */
-    public R visit(WhileStatement n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "System.out.println"
-     * f1 -> "("
-     * f2 -> Expression()
-     * f3 -> ")"
-     * f4 -> ";"
-     */
-    public R visit(PrintStatement n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> AndExpression()
-     *       | CompareExpression()
-     *       | PlusExpression()
-     *       | MinusExpression()
-     *       | TimesExpression()
-     *       | ArrayLookup()
-     *       | ArrayLength()
-     *       | MessageSend()
-     *       | PrimaryExpression()
-     */
-    public R visit(Expression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> PrimaryExpression()
-     * f1 -> "&&"
-     * f2 -> PrimaryExpression()
-     */
-    public R visit(AndExpression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> PrimaryExpression()
-     * f1 -> "<"
-     * f2 -> PrimaryExpression()
-     */
-    public R visit(CompareExpression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> PrimaryExpression()
-     * f1 -> "+"
-     * f2 -> PrimaryExpression()
-     */
-    public R visit(PlusExpression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> PrimaryExpression()
-     * f1 -> "-"
-     * f2 -> PrimaryExpression()
-     */
-    public R visit(MinusExpression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> PrimaryExpression()
-     * f1 -> "*"
-     * f2 -> PrimaryExpression()
-     */
-    public R visit(TimesExpression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> PrimaryExpression()
-     * f1 -> "["
-     * f2 -> PrimaryExpression()
-     * f3 -> "]"
-     */
-    public R visit(ArrayLookup n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> PrimaryExpression()
-     * f1 -> "."
-     * f2 -> "length"
-     */
-    public R visit(ArrayLength n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> PrimaryExpression()
-     * f1 -> "."
-     * f2 -> Identifier()
-     * f3 -> "("
-     * f4 -> ( ExpressionList() )?
-     * f5 -> ")"
-     */
-    public R visit(MessageSend n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> Expression()
-     * f1 -> ( ExpressionRest() )*
-     */
-    public R visit(ExpressionList n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> ","
-     * f1 -> Expression()
-     */
-    public R visit(ExpressionRest n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> IntegerLiteral()
-     *       | TrueLiteral()
-     *       | FalseLiteral()
-     *       | Identifier()
-     *       | ThisExpression()
-     *       | ArrayAllocationExpression()
-     *       | AllocationExpression()
-     *       | NotExpression()
-     *       | BracketExpression()
-     */
-    public R visit(PrimaryExpression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> <INTEGER_LITERAL>
-     */
-    public R visit(IntegerLiteral n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "true"
-     */
-    public R visit(TrueLiteral n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "false"
-     */
-    public R visit(FalseLiteral n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> <IDENTIFIER>
-     */
-    public R visit(Identifier n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "this"
-     */
-    public R visit(ThisExpression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "new"
-     * f1 -> "int"
-     * f2 -> "["
-     * f3 -> Expression()
-     * f4 -> "]"
-     */
-    public R visit(ArrayAllocationExpression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "new"
-     * f1 -> Identifier()
-     * f2 -> "("
-     * f3 -> ")"
-     */
-    public R visit(AllocationExpression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "!"
-     * f1 -> Expression()
-     */
-    public R visit(NotExpression n, A argu) {
-        return super.visit(n, argu);
-    }
-
-    /**
-     * f0 -> "("
-     * f1 -> Expression()
-     * f2 -> ")"
-     */
-    public R visit(BracketExpression n, A argu) {
+    public R visit(FormalParameter n, String argu) {
+        table.put(argu + n.f1.f0.tokenImage, new IdentifierType(n.f0));
         return super.visit(n, argu);
     }
 }
