4ebe0e6596652602a49e6d87fd8fe9355bd736b4
zacharyasmith <zach@medialusions.com>
2018-03-21 10:54:39 -0600

    Finished.

--- ECE456/lab5/client.py
+++ ECE456/lab5/client.py
@@ -7,10 +7,20 @@ Assignment: Lab 4
 
 import sys
 from tools import *
+import signal
 sys.path.append('./')
 
 
+def handler(signum, frame):
+    raise TimeoutException()
+
+
 class InvalidArgumentsError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class TimeoutException(Exception):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
 
@@ -26,28 +36,46 @@ if __name__ == "__main__":
         # clear prompt / welcome
         cls()
         print("Welcome! Opening file `{}`".format(file))
+        # what is the size of the file?
+        file_size = os.path.getsize(file)
         with open(file, 'r+b') as f:
             # create socket
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             # connect via IP address to set port, 5000
             s.connect((ip_address, 5000))
             # need to ask if can transfer
-            question = "Can I send file `{}` to you?".format(os.path.basename(file))
+            question = "Can I send file `{}` to you? [{} B]".format(os.path.basename(file), str(file_size))
             s.send(bytes(question, 'utf-8'))
             print("Asking server `{}`".format(question))
             response = s.recv(1024)
-            print("Response was `{}`".format(str(response)))
+            print("Response was `{}`".format(str(response, 'utf-8')))
+            # check response
             if response != b'OK':
                 print("Closing connection...")
                 s.close()
                 exit(0)
+            # begin file read and transfer
             data = f.read(1024)
             while data:
-                print("Read {} B from `{}`.\nSending...".format(os.path.getsize(file), file))
+                print("Read {} B from `{}`. Sending...".format(len(data), file))
                 s.send(data)
                 data = f.read(1024)
-            # Close connection after successful data send
-            print("Done. Closing connection...")
+            # Await acknowledgement
+            print("Done sending. Awaiting acknowledgement.")
+            try:
+                # Start timeout after sent
+                signal.signal(signal.SIGALRM, handler)
+                signal.alarm(5)
+                ack = True
+                while not ack:
+                    ack = s.read(1024)
+                    ack = ack == b'ACK'
+                # reset alarm
+                signal.alarm(0)
+                print("Acknowledgement received.")
+            except TimeoutException:
+                print("Timeout on awaiting acknowledgement.")
+            print("Closing connection...")
             s.close()
     except InvalidArgumentsError:
         print('Invalid use of program. Correct use:')
--- ECE456/lab5/server.py
+++ ECE456/lab5/server.py
@@ -6,6 +6,7 @@ Assignment: Lab 5
 """
 from tools import *
 import sys
+import re
 sys.path.append('./')
 
 
@@ -25,25 +26,49 @@ if __name__ == "__main__":
             client, address = s.accept()
             # accept data
             question = client.recv(1024)
-            print("Question was `{}`".format(str(question)))
+            print("Client asking `{}`".format(str(question, 'utf-8')))
             q_response = input("Proceed? [Y/n] ")
             if str(q_response).lower() not in ["", "y", "yes"]:
                 # not accepted
                 client.send(bytes('NO', 'utf-8'))
+                cls()
                 continue
+            # get file size from question
+            match = re.search(r'(?:\?\s\[)(\d+)(?:\sB\])', str(question, 'utf-8'))
+            if not match:
+                print("Regex error!")
+                exit(1)
+            else:
+                file_size = int(match.group(1))
             # proceed
             client.send(bytes('OK', 'utf-8'))
-            done = False
             file_data = bytes(0)
-            while not done:
-                # TODO better way to calculate done
-                # accept data
-                data = client.recv(1024)
-                file_data += data
+            # accept first chunk
+            data_count = 0
+            data = client.recv(1024)
+            while data:
+                # handle
+                data_count += len(data)
                 print("Received {} B".format(len(data)))
-                # done if
-                done = len(data) < 1024
+                file_data += data
+                data = client.recv(1024)
+            # send acknowledgement if full file was received
+            print("Received total of {} B and expected {} B.".format(data_count, file_size))
+            if data_count == file_size:
+                print("Sending acknowledgement.")
+            else:
+                print("Incomplete file received.")
+                continue
+            client.send(bytes('ACK', 'utf-8'))
+            # save to file
             file_name = input("Done receiving data.\nFilename to save as? ")
+            exists = os.path.isfile(file_name)
+            if exists:
+                overwrite = input("File exists. Overwrite? [Y/n] ")
+                if str(overwrite).lower() not in ["", "y", "yes"]:
+                    cls()
+                    print("Resetting...")
+                    continue
             with open(file_name, 'wb') as f:
                 f.write(file_data)
                 cls()
